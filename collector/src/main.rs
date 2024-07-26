2a97a6b1c952fc0c888f7087d396f17c9556ecf5
digitalscyther <digitalscyther@gmail.com>
2024-07-26 11:22:46 +0800

    add - redis for collection wallets need to check

--- collector/src/main.rs
+++ collector/src/main.rs
@@ -5,18 +5,31 @@ use amqprs::{callbacks::{DefaultChannelCallback, DefaultConnectionCallback}, cha
 use amqprs::channel::{BasicAckArguments, Channel};
 use async_trait::async_trait;
 use log::{info};
-use serde_json::Value;
+use redis::aio::MultiplexedConnection;
+use redis::{AsyncCommands, AsyncIter};
 use tokio::{task, time};
-use tokio::sync::Notify;
+use tokio::sync::{Notify};
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+use serde::{Deserialize, Serialize};
+use std::str;
+
+const PREFIX: &str = "wid:";
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Message {
+    address: String,
+    is_active: bool,
+    wallet_id: String,
+}
 
 struct MyConsumer {
     no_ack: bool,
+    redis: MultiplexedConnection,
 }
 
 impl MyConsumer {
-    pub fn new(no_ack: bool) -> Self {
-        Self { no_ack }
+    pub fn new(no_ack: bool, redis: MultiplexedConnection) -> Self {
+        Self { no_ack, redis }
     }
 }
 
@@ -29,9 +42,14 @@ impl AsyncConsumer for MyConsumer {
         _basic_properties: BasicProperties,
         content: Vec<u8>,
     ) {
-        time::sleep(time::Duration::from_secs(3)).await;
-        let data: Value = serde_json::from_slice(&content).unwrap();
-        info!("received: {:?}", data);
+        let msg: Message = serde_json::from_slice::<Message>(&content.clone()).unwrap().into();
+
+        let r_key = format!("{}{}", PREFIX, msg.wallet_id.clone());
+        if msg.is_active {
+            self.redis.set::<String, Vec<u8>, ()>(r_key, content).await.unwrap();
+        } else {
+            self.redis.del::<String, i64>(r_key).await.unwrap();
+        };
 
         if !self.no_ack {
             let args = BasicAckArguments::new(deliver.delivery_tag(), false);
@@ -42,27 +60,45 @@ impl AsyncConsumer for MyConsumer {
 
 #[tokio::main]
 async fn main() {
-    let t1 = task::spawn(async move {
-        rabbit_fn().await;
+    let redis_url = &env::var("REDIS_URL").unwrap_or("redis://127.0.0.1:6381/".to_string());
+    let redis = get_redis_con(redis_url).await.unwrap();
+
+    let r_clone = redis.clone();
+    let t2 = task::spawn(async move {
+        j_print(r_clone).await;
     });
 
-    let t2 = task::spawn(async move {
-        j_print().await;
+    let t1 = task::spawn(async move {
+        rabbit_fn(redis).await;
     });
 
     let _ = tokio::join!(t1, t2);
 }
 
-async fn j_print() {
+async fn j_print(mut redis: MultiplexedConnection) {
     let mut c = 0;
     loop {
-        info!("j_print: {}", c);
+        let pattern = format!("{}*", PREFIX);
+        let mut values: Vec<String> = vec!();
+        let mut iterator: AsyncIter<String> = redis.scan_match(pattern).await.unwrap();
+        while let Some(item) = iterator.next_item().await {
+            values.push(item)
+        }
+
+        if values.is_empty() {
+            info!("[{}] Current map is empty", c);
+        } else {
+            info!("[{}] Current map contents:", c);
+            for val in values.iter() {
+                info!("{:?}", val);
+            }
+        }
         c += 1;
-        time::sleep(time::Duration::from_secs(10*60)).await;
+        time::sleep(time::Duration::from_secs(5)).await;
     }
 }
 
-async fn rabbit_fn() {
+async fn rabbit_fn(redis: MultiplexedConnection) {
     tracing_subscriber::registry()
         .with(fmt::layer())
         .with(EnvFilter::from_default_env())
@@ -75,8 +111,8 @@ async fn rabbit_fn() {
         "guest",
         "guest",
     ))
-    .await
-    .unwrap();
+        .await
+        .unwrap();
     connection
         .register_callback(DefaultConnectionCallback)
         .await
@@ -110,10 +146,17 @@ async fn rabbit_fn() {
     let args = BasicConsumeArguments::new(&queue_name, "example_basic_pub_sub");
 
     channel
-        .basic_consume(MyConsumer::new(args.no_ack), args)
+        .basic_consume(MyConsumer::new(args.no_ack, redis), args)
         .await
         .unwrap();
 
     let guard = Notify::new();
     guard.notified().await;
+}
+
+async fn get_redis_con(url: &str) -> Result<MultiplexedConnection, String> {
+    let client = redis::Client::open(url).unwrap();
+    client.get_multiplexed_async_connection().await.map_err(
+        |err| format!("Failed create connection:\n{:?}", err)
+    )
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 run local
 ```shell
-docker compose build && POSTGRES_PORT="5438" RABBITMQ_PORT="5672" API_PORT="3001" HDWALLET_PORT="8001" docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+docker compose build && POSTGRES_PORT="5438" RABBITMQ_PORT="5672" REDIS_PORT="6381" API_PORT="3001" HDWALLET_PORT="8001" docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d
 ```
